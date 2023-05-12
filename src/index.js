a20901546e1cd8681216bba00a8a0fccb4286d20
Lucas <lucascarlim0520@gmail.com>
2023-05-12 11:16:28 -0300

    final commit

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocs = require('./swagger.json');
+const cors = require('cors');
 
 async function open (uri) {
   await mongoose.connect(uri);
 }
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 

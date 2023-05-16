0f9a58b78be5874b44eb78004f79ae05902b3098
Lucas <lucascarlim0520@gmail.com>
2023-05-16 15:30:35 -0300

    fix: wrong name

--- src/services/player/CreatePlayerService.js
+++ src/services/player/CreatePlayerService.js
@@ -1,6 +1,9 @@
 const PlayerModel = require('../../database/model/PlayerModel');
 
 module.exports = async ({ user_id, filename, nickname, gender, combat_class, description }) => {
+  const nameExists = await PlayerModel.findOne({ nickname });
+  if (nameExists) throw new Error('Nickname already exists!');
+
   const player = await PlayerModel.create({
     nickname,
     filename,

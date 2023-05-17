32a3d6b0ff8964eee9521deaaae584138145ea51
Lucas <lucascarlim0520@gmail.com>
2023-05-17 10:15:05 -0300

    fix: multer config

--- src/config/PlayerMulterConfig.js
+++ src/config/PlayerMulterConfig.js
@@ -3,7 +3,7 @@ const crypto = require('crypto');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './temp/player');
+    cb(null, 'temp/player');
   },
   filename: function (req, file, cb) {
     crypto.randomBytes(16, (err, buf) => {

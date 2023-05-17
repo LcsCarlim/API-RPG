9958ebd840e8eccf19851bf058dcaabd8d917892
Lucas <lucascarlim0520@gmail.com>
2023-05-17 15:49:30 -0300

    feat: aws s3

--- src/config/CharacterMulterConfig.js
+++ src/config/CharacterMulterConfig.js
@@ -1,19 +1,59 @@
 const multer = require('multer');
+const path = require('path');
 const crypto = require('crypto');
+const aws = require('aws-sdk');
+const multerS3 = require('multer-s3');
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './temp/uploads');
+const storageTypes = {
+  local: multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, path.resolve(__dirname, '..', '..', 'temp', 'uploads'));
+    },
+    filename: (req, file, cb) => {
+      crypto.randomBytes(16, (err, hash) => {
+        if (err) cb(err);
+
+        file.key = `${hash.toString('hex')}-${file.originalname}`;
+
+        cb(null, file.key);
+      });
+    }
+  }),
+  s3: multerS3({
+    s3: new aws.S3(),
+    bucket: process.env.BUCKET_NAME,
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: 'public-read',
+    key: (req, file, cb) => {
+      crypto.randomBytes(16, (err, hash) => {
+        if (err) cb(err);
+
+        const fileName = `${hash.toString('hex')}-${file.originalname}`;
+
+        cb(null, fileName);
+      });
+    }
+  })
+};
+
+module.exports = {
+  dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
+  storage: storageTypes[process.env.STORAGE_TYPE],
+  limits: {
+    fileSize: 2 * 1024 * 1024
   },
-  filename: function (req, file, cb) {
-    crypto.randomBytes(16, (err, buf) => {
-      if (err) return cb(err);
-      const filename = buf.toString('hex') + '-' + file.originalname;
-      return cb(null, filename);
-    });
+  fileFilter: (req, file, cb) => {
+    const allowedMimes = [
+      'image/jpeg',
+      'image/pjpeg',
+      'image/png',
+      'image/gif'
+    ];
+
+    if (allowedMimes.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new Error('Invalid file type.'));
+    }
   }
-});
-
-const upload = multer({ storage });
-
-module.exports = upload;
+};

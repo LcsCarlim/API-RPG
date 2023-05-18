2fcb42736745698c16d99bb446487284570cfa68
Lucas <lucascarlim0520@gmail.com>
2023-05-18 13:47:34 -0300

    fix: multer config

--- src/config/PlayerMulterConfig.js
+++ src/config/PlayerMulterConfig.js
@@ -1,45 +1,26 @@
-const multer = require('multer');
-const path = require('path');
-const crypto = require('crypto');
 const aws = require('aws-sdk');
+const multer = require('multer');
 const multerS3 = require('multer-s3');
 
-const s3 = new aws.S3();
-const storage = multerS3({
-  s3,
-  bucket: process.env.BUCKET_NAME,
-  contentType: multerS3.AUTO_CONTENT_TYPE,
-  acl: 'public-read',
-  key: (req, file, cb) => {
-    crypto.randomBytes(16, (err, hash) => {
-      if (err) cb(err);
-
-      const fileName = `${hash.toString('hex')}-${file.originalname}`;
-
-      cb(null, fileName);
-    });
-  }
+aws.config.update({
+  accessKeyId: 'YOUR_ACCESS_KEY',
+  secretAccessKey: 'YOUR_SECRET_ACCESS_KEY',
+  region: 'YOUR_AWS_REGION'
 });
+
+const s3 = new aws.S3();
 
 const upload = multer({
-  storage,
-  limits: {
-    fileSize: 2 * 1024 * 1024
-  },
-  fileFilter: (req, file, cb) => {
-    const allowedMimes = [
-      'image/jpeg',
-      'image/pjpeg',
-      'image/png',
-      'image/gif'
-    ];
-
-    if (allowedMimes.includes(file.mimetype)) {
-      cb(null, true);
-    } else {
-      cb(new Error('Invalid file type.'));
+  storage: multerS3({
+    s3,
+    bucket: 'YOUR_S3_BUCKET_NAME',
+    acl: 'public-read',
+    key: function (req, file, cb) {
+      cb(null, Date.now().toString());
     }
-  }
+  })
 });
 
-module.exports = upload;
+module.exports = {
+  uploadImage: upload.single('image')
+};

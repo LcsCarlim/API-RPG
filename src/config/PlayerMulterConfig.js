6742e43ccffad6267adf4c7551cc86ee245df039
Lucas <lucascarlim0520@gmail.com>
2023-05-17 21:19:07 -0300

    fix: multer config

--- src/config/PlayerMulterConfig.js
+++ src/config/PlayerMulterConfig.js
@@ -37,7 +37,7 @@ const storageTypes = {
 };
 
 module.exports = {
-  dest: path.resolve('./temp/player'),
+  dest: path.resolve(__dirname, './temp/player'),
   storage: storageTypes[process.env.STORAGE_TYPE],
   limits: {
     fileSize: 2 * 1024 * 1024

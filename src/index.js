d71a3c4c3376fde6922f1be9944958ece1288365
Lucas <lucascarlim0520@gmail.com>
2023-05-17 21:57:32 -0300

    fix

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ const routes = require('./routes');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocs = require('./swagger.json');
 const cors = require('cors');
-const morgan = require('morgan');
 
 async function open (uri) {
   await mongoose.connect(uri);
@@ -19,9 +18,6 @@ app.use(express.json());
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs, { customCssUrl: CSS_URL }));
 
 app.use(cors());
-
-app.use(express.urlencoded({ extended: true }));
-app.use(morgan('dev'));
 
 app.use(routes);
 

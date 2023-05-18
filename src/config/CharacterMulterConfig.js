71a196e8d069d8bb7f46da51066b98a973280f36
Lucas <lucascarlim0520@gmail.com>
2023-05-17 22:13:38 -0300

    fix

--- src/config/CharacterMulterConfig.js
+++ src/config/CharacterMulterConfig.js
@@ -37,7 +37,7 @@ const storageTypes = {
 };
 
 module.exports = {
-  dest: path.resolve('/temp/upload'),
+  dest: '/temp/upload',
   storage: storageTypes[process.env.STORAGE_TYPE],
   limits: {
     fileSize: 2 * 1024 * 1024

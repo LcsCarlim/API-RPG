a6ad2404ea2681dd5bfae5c60258a4b7f1a72613
Lucas <lucascarlim0520@gmail.com>
2023-05-29 15:43:35 -0300

    fix: remove friend

--- src/services/user/RemoveFriendService.js
+++ src/services/user/RemoveFriendService.js
@@ -1,15 +1,11 @@
 const UserModel = require('../../database/model/UserModel');
 
 module.exports = async (id, friendId) => {
-  const [user, friendIsValid] = await Promise.all([
-    UserModel.findById(friendId)
-  ]);
-
-  if (!friendIsValid) throw new Error('Friend not exists');
+  const user = await UserModel.findById(id);
 
   const friendExists = user.friends.find((friend) => friend.toString() === friendId.toString());
 
-  if (friendExists) throw new Error('Friend not exists!');
+  if (!friendExists) throw new Error('Friend not exists!');
 
   const removeFriend = await UserModel.findByIdAndUpdate(id, { $pull: { friends: friendId } }, { new: true });
 
